5fe350a5cd79d24e0fcbbf6cc06d3547abd37dd5
Ch4nYH <hfercxx@hotmail.com>
2021-08-16 23:07:19 +0800

    updatee

--- multilstm/scripts/0815.sh
+++ multilstm/scripts/0815.sh
@@ -33,8 +33,7 @@ done
 
 
 
-git checkout multilstm
-git pull
+
 CUDA_VISIBLE_DEVICES=1 nohup python train_rnnprop.py --save_path=mnist_multilstm_cl_rnnprop --problem=mnist --if_cl=True --if_mt=False --num_epochs=10000 --num_steps=100 --seed 1 > multilstm_rnnprop.out &
 
 
