aaad0520e56da67bf50e119358ffde37330064c7
Ch4nYH <hfercxx@hotmail.com>
2021-08-17 22:26:30 +0800

    update

--- multilstm/scripts/0816_train_rnnprop.sh
+++ multilstm/scripts/0816_train_rnnprop.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python train_rnnprop.py --save_path=mnist_multilstm_cl_rnnprop --problem=mnist --if_cl=True --if_mt=False --num_epochs=10000 --num_steps=100
+CUDA_VISIBLE_DEVICES=1 python train_rnnprop.py --save_path=mnist_multilstm_cl_rnnprop_5 --problem=mnist --if_cl=True --if_mt=False --num_epochs=10000 --num_steps=100
