a9d1bdf517f88c0dfee9b1f1020ab84606b40364
Ch4nYH <hfercxx@hotmail.com>
2021-08-17 23:29:46 +0800

    update

--- multilstm/scripts/0816_multilstm_cl.sh
+++ multilstm/scripts/0816_multilstm_cl.sh
@@ -1,3 +1,3 @@
-for i in $(seq 1 1); do
+for i in $(seq 1 5); do
 CUDA_VISIBLE_DEVICES=0 python evaluate_rnnprop.py --path=mnist_multilstm_cl_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=10000 --output_path mnist_multilstm_cl_rnnprop_5_eval_${i} --seed ${i} 
 done
