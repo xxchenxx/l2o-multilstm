55db1988a4b99b1c425c8b54a1326eb9e0e1e9ef
Ch4nYH <hfercxx@hotmail.com>
2021-08-17 13:27:57 +0800

    update

--- multilstm/examine.py
+++ multilstm/examine.py
@@ -4,6 +4,5 @@ a = pickle.load(open("mnist_multilstm_cl_rnnprop/rp.l2l-0", 'rb'))
 
 b = pickle.load(open("mnist_multilstm_cl_rnnprop/rp.l2l-1000", 'rb'))
 
-print(a)
-
-print(b)
+for key in a:
+    print(a[key] - b[key])
