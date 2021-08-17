3799503c3c1aad9e3b9b0b6d7ccb10f28dcc03b0
Ch4nYH <hfercxx@hotmail.com>
2021-08-18 00:39:23 +0800

    update

--- original/scripts/0816.sh
+++ original/scripts/0816.sh
@@ -1,1 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 nohup python train_rnnprop.py --save_path=mnist_original_cl_rnnprop --problem=mnist --if_cl=True --if_mt=False --num_epochs=10000 --num_steps=100 --seed 1 > original_rnnprop.out &
+
+CUDA_VISIBLE_DEVICES=0 python train_rnnprop.py --save_path=mnist_original_cl_rnnprop --problem=mnist --if_cl=True --if_mt=False --num_epochs=10000 --num_steps=100 --seed 1 
