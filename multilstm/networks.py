8211367c8653092e79e79244145493a2e924f1ee
Ch4nYH <hfercxx@hotmail.com>
2021-08-17 12:20:07 +0800

    updatee

--- multilstm/networks.py
+++ multilstm/networks.py
@@ -182,7 +182,7 @@ class StandardDeepLSTM(Network):
     self._scale = scale
     self._preprocess_name = preprocess_name
     self.counter = 0
-    self.num_lstm = 2
+    self.num_lstm = 1
     #self.index = tf.placeholder(tf.int32, (1), name='index')
     
     if preprocess_name == 'fc':
