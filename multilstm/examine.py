c2b218161d33012885413e22d209feb53c8e2557
Ch4nYH <hfercxx@hotmail.com>
2021-08-17 15:17:57 +0800

    update

--- multilstm/examine.py
+++ multilstm/examine.py
@@ -8,4 +8,4 @@ for key in a:
     if (isinstance(a[key], dict)):
         for key2 in a[key]:
             
-            print(key + ":"  + key2, (a[key][key2] - b[key][key2]).abs().mean())
+            print(key + ":"  + key2, (np.abs(a[key][key2] - b[key][key2])).mean())
