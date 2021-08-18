359a21e4dfce2b1354f9d10f215b6766926e9320
Ch4nYH <hfercxx@hotmail.com>
2021-08-18 15:49:04 +0800

    update

--- multilstm/train_dm.py
+++ multilstm/train_dm.py
@@ -61,7 +61,7 @@ flags.DEFINE_integer("k", 1, "")
 
 
 def main(_):
-
+    tf.set_random_seed(1)
     os.environ['TF_CUDNN_DETERMINISTIC']='1'
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'  # or any {'0', '1', '2'}
 
