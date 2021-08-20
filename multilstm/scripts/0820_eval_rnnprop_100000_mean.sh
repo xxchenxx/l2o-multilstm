6e8f7b251bc798b21bbf8a9d6f74d6eae80d79e4
Ch4nYH <hfercxx@hotmail.com>
2021-08-20 21:34:48 +0800

    update

--- multilstm/scripts/0820_eval_rnnprop_100000_mean.sh
+++ multilstm/scripts/0820_eval_rnnprop_100000_mean.sh
@@ -1,3 +1,3 @@
 for i in $(seq 1 5); do
-CUDA_VISIBLE_DEVICES=0 python evaluate_rnnprop_mean.py --path=mnist_multilstm_cl_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=10000 --output_path mnist_multilstm_cl_rnnprop_mean_5_eval_${i} --seed ${i} 
+CUDA_VISIBLE_DEVICES=0 python evaluate_rnnprop_mean.py --path=mnist_multilstm_cl_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=100000 --output_path mnist_multilstm_cl_rnnprop_mean_100000_5_eval_${i} --seed ${i} 
 done
