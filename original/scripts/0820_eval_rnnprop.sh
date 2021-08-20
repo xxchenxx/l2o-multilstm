78704c0f7c96addaff4cdaa24aecf6f6fbdee195
Ch4nYH <hfercxx@hotmail.com>
2021-08-20 23:14:54 +0800

    updateee

---
+++ original/scripts/0820_eval_rnnprop.sh
@@ -0,0 +1,3 @@
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=1 python evaluate_rnnprop.py --path=mnist_original_rnnprop/rp.l2l-0 --problem=mnist --num_steps=10000 --output_path mnist_original_rnnprop_eval_10000_${i} --seed ${i} 
+done
