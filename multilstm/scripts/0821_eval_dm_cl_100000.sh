daeda747dced3b2f7b19fdb38096d8c77ae4ee3b
Ch4nYH <hfercxx@hotmail.com>
2021-08-21 17:24:21 +0800

    update

--- multilstm/scripts/0821_eval_dm_cl_100000.sh
+++ multilstm/scripts/0821_eval_dm_cl_100000.sh
@@ -1,3 +1,3 @@
 for i in $(seq 1 5); do
-CUDA_VISIBLE_DEVICES=1 python evaluate_dm_mean.py --path=mnist_multilstm_dm_cl_5/cw.l2l-0 --problem=mnist --num_steps=10000 --output_path mnist_multilstm_dm_cl_5_mean_eval_${i} --seed ${i} 
+CUDA_VISIBLE_DEVICES=1 python evaluate_dm_mean.py --path=mnist_multilstm_cl_dm_5/cw.l2l-0 --problem=mnist --num_steps=100000 --output_path mnist_multilstm_cl_dm_5_mean_100000_eval_${i} --seed ${i} 
 done
