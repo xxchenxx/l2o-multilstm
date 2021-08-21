de40533e6c7021957f9f36cf1b036f8001ccff17
Ch4nYH <hfercxx@hotmail.com>
2021-08-21 16:15:20 +0800

    update

---
+++ multilstm/scripts/0821_eval_dm_cl.sh
@@ -0,0 +1,3 @@
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=1 python evaluate_dm_mean.py --path=mnist_multilstm_dm_cl_5/cw.l2l-0 --problem=mnist --num_steps=10000 --output_path mnist_multilstm_dm_cl_5_mean_eval_${i} --seed ${i} 
+done
