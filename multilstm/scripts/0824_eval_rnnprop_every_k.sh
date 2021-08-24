1f3b5bd04fe0a6dd5adf0ecbb5b6806aeb8e3229
Ch4nYH <hfercxx@hotmail.com>
2021-08-24 10:02:16 +0800

    updatee

---
+++ multilstm/scripts/0824_eval_rnnprop_every_k.sh
@@ -0,0 +1,32 @@
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=1 python evaluate_rnnprop.py --path=mnist_multilstm_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=10000 --output_path 0821_mnist_multilstm_rnnprop_5_eval_${i} --seed ${i} 
+done
+
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=1 python evaluate_rnnprop.py --path=mnist_multilstm_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=100000 --output_path 0821_mnist_multilstm_rnnprop_5_100000_eval_${i} --seed ${i} 
+done
+
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=1 python evaluate_rnnprop_mean.py --path=mnist_multilstm_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=10000 --output_path 0821_mnist_multilstm_rnnprop_5_mean_eval_${i} --seed ${i} 
+done
+
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=1 python evaluate_rnnprop_mean.py --path=mnist_multilstm_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=100000 --output_path 0821_mnist_multilstm_rnnprop_5_100000_mean_eval_${i} --seed ${i} 
+done
+
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=1 python evaluate_rnnprop.py --path=mnist_multilstm_cl_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=10000 --output_path 0821_mnist_multilstm_cl_rnnprop_5_eval_${i} --seed ${i} 
+done
+
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=1 python evaluate_rnnprop.py --path=mnist_multilstm_cl_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=100000 --output_path 0821_mnist_multilstm_cl_rnnprop_5_100000_eval_${i} --seed ${i} 
+done
+
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=1 python evaluate_rnnprop_mean.py --path=mnist_multilstm_cl_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=10000 --output_path 0821_mnist_multilstm_cl_rnnprop_5_mean_eval_${i} --seed ${i} 
+done
+
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=1 python evaluate_rnnprop_mean.py --path=mnist_multilstm_cl_rnnprop_5/rp.l2l-0 --problem=mnist --num_steps=100000 --output_path 0821_mnist_multilstm_cl_rnnprop_5_100000_mean_eval_${i} --seed ${i} 
+done
+
