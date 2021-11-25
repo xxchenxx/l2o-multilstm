c70ad39a4e6126aebe16909f0b08e3b5638cceeb
Ch4nYH <hfercxx@hotmail.com>
2021-11-25 20:27:41 +0800

    update

---
+++ multilstm/scripts/1125.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=0 nohup python train_rnnprop.py --save_path=mnist_multilstm_rnnprop_5_step100 --problem=mnist --if_cl=False --if_mt=False --num_epochs=10000 --num_steps=100 > 1125_train_rnnprop.out &
+
+CUDA_VISIBLE_DEVICES=1 nohup python train_rnnprop.py --save_path=mnist_multilstm_rnnprop_5_step100 --problem=mnist --if_cl=True --if_mt=False --num_epochs=10000 --num_steps=100 > 1125_train_rnnprop_cl.out &
---
+++ multilstm/scripts/1125_eval_rnnprop_mean.sh
@@ -0,0 +1,3 @@
+for i in $(seq 1 5); do
+CUDA_VISIBLE_DEVICES=0 python evaluate_rnnprop_mean.py --path=mnist_multilstm_rnnprop_5_step100/rp.l2l-0 --problem=mnist --num_steps=10000 --output_path mnist_multilstm_rnnprop_5_step100_mean_eval_${i} --seed ${i} 
+done
